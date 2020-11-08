a918cac3065a383710ae2f96797009b40e614fe3
prapasanan <57786048+prapasanan@users.noreply.github.com>
2020-11-08 19:43:27 +0700

    Initial commit

---
+++ oop_prapasanan/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_prapasanan
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            teacher myteacher = new teacher();
+            myteacher.uniform();
+            myteacher.salary();
+            nurse mynurse = new nurse();
+            mynurse.uniform();
+            mynurse.salary();
+            Pharmacist myPharmacist = new Pharmacist();
+            myPharmacist.uniform();
+            myPharmacist.Salary();
+
+        }
+    }
+}
---
+++ oop_prapasanan/career.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_prapasanan
+{
+    class career
+    {
+        public string uniform;
+        public string WorkClothes;
+        public string salary;
+    }
+    class teacher : career
+    {
+        private void WorkClothes()
+        {
+
+        }
+        public void uniform()
+        {
+            Console.WriteLine("Official uniform");
+
+        }
+        public void salary()
+        {
+            Console.WriteLine("15000");
+        }
+
+    }
+    class nurse : career
+    {
+        private void WorkClothes()
+        {
+
+        }
+        public void uniform()
+        {
+            Console.WriteLine("Nurse uniform");
+        }
+        public void salary()
+        {
+            Console.WriteLine("22,200");
+        }
+    }
+    class Pharmacist : career
+    {
+        private void WorkClothes()
+        {
+
+        }
+        public void uniform()
+        {
+            Console.WriteLine("Pharmacist uniform");
+        }
+        public void Salary()
+        {
+            Console.WriteLine("25,000");
+        }
+
+        
+    }
+}
